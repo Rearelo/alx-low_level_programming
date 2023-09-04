2861f6652edf79094081a0f95aaa5ed219f6659d
Rearelo <rearelo.chokwe@gmail.com>
2023-09-04 03:43:39 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianess - checks if a machine is little or bog endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
